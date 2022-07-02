292f43e3b559f83c96928143b0b92d3f82e23102
Rabah <rmerzoug471@gmail.com>
2022-07-02 10:53:47 -0500

    commit tree 71730bcc794f0ce88fdadb11bbb66e79f26c49a9

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers() {
+    const returnFirstTwoDrivers = function (drivers) {
+    return drivers.splice(0, 2);
+    }
+    return returnFirstTwoDrivers;
+}
+function returnLastTwoDrivers() {
+    const returnLastTwoDrivers = function (drivers){
+    return drivers.splice(2, drivers.length - 1);
+    }
+}
